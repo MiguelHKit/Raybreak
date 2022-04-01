6e81313298024684d02f4f3ee95137ef8d30378a
piktamine <49212598+piktamine@users.noreply.github.com>
2022-04-01 14:38:40 -0600

    V0.0

--- Raybreak/ViewController.swift
+++ Raybreak/ViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import MetalKit
+
+enum metalColors{
+    static let wenderlichGreen = MTLClearColor(red: 0.0, green: 0.4, blue: 0.21, alpha: 1.0)
+}
 
 class ViewController: UIViewController {
+    
+    var metalView: MTKView{
+        return view as! MTKView
+    }
+    
+    var device: MTLDevice!
+    var commandQueue: MTLCommandQueue!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        metalView.device = MTLCreateSystemDefaultDevice()
+        device = metalView.device
+        
+        metalView.clearColor = metalColors.wenderlichGreen
+        
+        commandQueue = device.makeCommandQueue()
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        
+        let commandEncoder = commandBuffer
     }
 
 
